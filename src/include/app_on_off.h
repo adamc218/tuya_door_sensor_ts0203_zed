b7c7933039f25e8dfbbffea6f30230922dd78428
Fedor <lfa1@yandex.ru>
2026-03-19 20:02:59 +0300

    Update app_on_off.h

--- src/include/app_on_off.h
+++ src/include/app_on_off.h
@@ -1,24 +1,24 @@
 #ifndef SRC_INCLUDE_APP_ON_OFF_H_
 #define SRC_INCLUDE_APP_ON_OFF_H_
 
-#define DEV_ONOFF_CMD_NUM   (APS_GROUP_TABLE_NUM+APS_BINDING_TABLE_NUM)
-
-typedef struct {
-    uint8_t  used :1;
-    uint8_t  cmd_send :1;
-    uint8_t  status_rsp :1;
-    uint8_t  group :1;
-    uint8_t  command :2;
-    uint8_t  cnt :2;
-    uint8_t  seq_num;
-    uint16_t group_addr;
-} dev_onoff_cmd_t;
-
-enum {
-    ONOFF_CMD_SINGLE = 0,
-    ONOFF_CMD_CHECK_ANSWER,
-    ONOFF_CMD_REPEAT
-};
+//#define DEV_ONOFF_CMD_NUM   (APS_GROUP_TABLE_NUM+APS_BINDING_TABLE_NUM)
+//
+//typedef struct {
+//    uint8_t  used :1;
+//    uint8_t  cmd_send :1;
+//    uint8_t  status_rsp :1;
+//    uint8_t  group :1;
+//    uint8_t  command :2;
+//    uint8_t  cnt :2;
+//    uint8_t  seq_num;
+//    uint16_t group_addr;
+//} dev_onoff_cmd_t;
+//
+//enum {
+//    ONOFF_CMD_SINGLE = 0,
+//    ONOFF_CMD_CHECK_ANSWER,
+//    ONOFF_CMD_REPEAT
+//};
 
 void cmdOnOff(uint8_t command);
 //void dev_onoff_cmd_init();
